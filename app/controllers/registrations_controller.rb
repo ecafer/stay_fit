3803721195c99e7017d712353dc54f3984abf228
Emirali Caferzade <ecaferzade@hotmail.com>
2023-11-20 16:47:51 +0100

    destroyed and created new controllers

--- app/controllers/registrations_controller.rb
+++
@@ -1,4 +0,0 @@
-class RegistrationsController < ApplicationController
-    def show
-    end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  
-  get "/", to: "homepages#show"
+  root to: "home#index"
 
-  get "/signup", to: "registrations#show"
 end
